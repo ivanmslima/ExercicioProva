411412dfd638240d1ab8cf436b501b80657a4d26
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-26 21:27:53 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioProva/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Digite um número:  ");
+
+            int nun, maior = 0;
+
+            for (int i= 0; i < 10; i++)
+            {
+                Console.WriteLine("Informe um número: ");
+                    nun = int.Parse(Console.ReadLine());
+
+                if(nun > maior)
+                {
+                    maior = nun;
+                }
+            }
+            Console.WriteLine($"O maior número é {maior}");
+
+
+            
+
+
+        }
+    }
+}
